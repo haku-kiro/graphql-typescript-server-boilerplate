ada131acfc708736eb19478be8b6b06f49b4bc7e
haku-kiro <botPrime@yahoo.com>
2020-11-14 23:00:10 +0200

    Adding register user logic, basic typeorm based stuff

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,11 +1,26 @@
 import { ResolverMap } from "./types/graphql-utils";
+import * as bcrypt from "bcryptjs";
+import { User } from "./entity/User";
 
 export const resolvers: ResolverMap = {
   Query: {
-    hello: (_, { name }: GQL.IHelloOnQueryArguments) => `Hello ${name || "World!"}`
+    hello: (_, { name }: GQL.IHelloOnQueryArguments) =>
+      `Hello ${name || "World!"}`,
   },
   Mutation: {
     // Just so that the email, and password is being used
-    register: (_, { email, password }: GQL.IRegisterOnMutationArguments) => `Bye ${email || password || "World"}`
-  }
+    register: async (_, { email, password }: GQL.IRegisterOnMutationArguments) => {
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // creates an object in memory, that you can do stuff with,
+      // Make sure to save the user
+      const user = await User.create({
+        email,
+        password: hashedPassword,
+      });
+
+      await user.save();
+      return true;
+    },
+  },
 };
